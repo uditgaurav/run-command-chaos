113384a1403dd303554aeb8de24cd2b909bf747a
Jonsy13 <vedant.shrotria@chaosnative.com>
2022-02-01 02:17:47 +0530

    Adds script for installing prerequisite packages
    Signed-off-by: Jonsy13 <vedant.shrotria@chaosnative.com>

--- pre-setup.sh
+++ pre-setup.sh
@@ -6,26 +6,26 @@
 declare -a pkgs=("iproute-tc" "stress-ng")
 
 ## Verifying & loading sch_netem package
-if lsmod | grep "sch_netem"
+if [[ ! -z $(lsmod | grep "sch_netem") ]]
 then
     echo -e "\n[Info]: sch_netem module is already loaded\n"
 else
     echo -e "\n[Info]: sch_netem module not loaded, checking if kernel-modules-extra package is installed...\n"
-    if rpm -q kernel-modules-extra-$(uname -r)
+    if [[ ! -z $(rpm -aq kernel-modules-extra-$(uname -r)) ]]
     then
         echo -e "\n[Info]: kernel-modules-extra package is already installed\n"
     else
         echo -e "\n[Info]: kernel-modules-extra package NOT installed, Installing now..."
         sudo dnf install kernel-modules-extra-$(uname -r)
     fi
-    echo -e "\n Loading sch_netem kernel module..."
+    echo -e "\n[Info]: Loading sch_netem kernel module..."
     sudo modprobe sch_netem
 fi
 
 ## verifying & installing all required packages
-for pkg in $pkgs
+for pkg in "${pkgs[@]}"
 do
-    if rpm -q $pkg
+    if [[ ! -z $(rpm -aq $pkg) ]]
     then
         echo -e "\n[Info]: $pkg is already installed\n"
     else
