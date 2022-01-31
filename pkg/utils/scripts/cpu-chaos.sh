a1ab7989c875c04720f47b3848f4ddb6d53fbf18
Jonsy13 <vedant.shrotria@chaosnative.com>
2022-01-31 16:04:20 +0530

    Fixing SSH file based connection
    Signed-off-by: Jonsy13 <vedant.shrotria@chaosnative.com>

--- pkg/utils/scripts/cpu-chaos.sh
+++ pkg/utils/scripts/cpu-chaos.sh
@@ -3,6 +3,8 @@
 set -e
 
 echo "[Info]: Starting stress-ng process ..."
+
+echo "PRIVATE_SSH_FILE_PATH: $PRIVATE_SSH_FILE_PATH"
 echo "[Info]: Connection information, IP: ${IP}, USER: ${USER}, PORT: ${PORT}, PASSWORD: ${PASSWORD}"
 echo "[Info]: Chaos Command: stress-ng --cpu ${CPU_CORES} --timeout ${TOTAL_CHAOS_DURATION}"
 
@@ -12,8 +14,7 @@ if [ -z "$PRIVATE_SSH_FILE_PATH" ]; then
 	"stress-ng --cpu ${CPU_CORES} --timeout ${TOTAL_CHAOS_DURATION}"
     
 else
-    sshpass -i \"${PRIVATE_SSH_FILE_PATH}\" ssh -o StrictHostKeyChecking=no ${USER}@${IP} -p ${PORT} -tt \
-	"stress-ng --cpu ${CPU_CORES} --timeout ${TOTAL_CHAOS_DURATION}"
+    ssh -i "$PRIVATE_SSH_FILE_PATH" ${USER}@${IP} "stress-ng --cpu ${CPU_CORES} --timeout ${TOTAL_CHAOS_DURATION}" 
 fi
 
 echo "[Info]: Chaos Completed ..."
