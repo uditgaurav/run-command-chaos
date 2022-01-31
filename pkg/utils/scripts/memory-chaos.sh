83cc6d5d3e0afe13ac108abf89b13046fecec4b8
Jonsy13 <vedant.shrotria@chaosnative.com>
2022-02-01 00:00:13 +0530

    Adds workflows for all experiments
    Signed-off-by: Jonsy13 <vedant.shrotria@chaosnative.com>

--- pkg/utils/scripts/memory-chaos.sh
+++ pkg/utils/scripts/memory-chaos.sh
@@ -6,15 +6,15 @@ echo "[Info]: Starting stress-ng process for Memory chaos..."
 
 echo "PRIVATE_SSH_FILE_PATH: $PRIVATE_SSH_FILE_PATH"
 echo "[Info]: Connection information, IP: ${IP}, USER: ${USER}, PORT: ${PORT}"
-echo "[Info]: Chaos Command: stress-ng --vm ${NUMBER_OF_WORKERS} --vm-bytes ${MEMORY_CONSUMPTION} --timeout ${TOTAL_CHAOS_DURATION}"
+echo "[Info]: Chaos Command: stress-ng --vm ${NUMBER_OF_WORKERS} --vm-bytes ${MEMORY_CONSUMPTION}M --timeout ${TOTAL_CHAOS_DURATION}"
 
 if [ -z "$PRIVATE_SSH_FILE_PATH" ]; then
 
     sshpass -p ${PASSWORD} ssh -o StrictHostKeyChecking=no ${USER}@${IP} -p ${PORT} -tt \
-	"stress-ng --vm ${NUMBER_OF_WORKERS} --vm-bytes ${MEMORY_CONSUMPTION} --timeout ${TOTAL_CHAOS_DURATION}"
+	"stress-ng --vm ${NUMBER_OF_WORKERS} --vm-bytes ${MEMORY_CONSUMPTION}M --timeout ${TOTAL_CHAOS_DURATION}"
 
 else
-    ssh -o StrictHostKeyChecking=no -i "$PRIVATE_SSH_FILE_PATH" ${USER}@${IP} "stress-ng --vm ${NUMBER_OF_WORKERS} --vm-bytes ${MEMORY_CONSUMPTION} --timeout ${TOTAL_CHAOS_DURATION}" 
+    ssh -o StrictHostKeyChecking=no -i "$PRIVATE_SSH_FILE_PATH" ${USER}@${IP} "stress-ng --vm ${NUMBER_OF_WORKERS} --vm-bytes ${MEMORY_CONSUMPTION}M --timeout ${TOTAL_CHAOS_DURATION}" 
 fi
 
 echo "[Info]: Chaos Completed ..."
