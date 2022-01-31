bc0d32041d3887cd17204cd9f2b8a4e7007731ce
Jonsy13 <vedant.shrotria@chaosnative.com>
2022-02-01 00:10:41 +0530

    Adds workflows for all experiments
    Signed-off-by: Jonsy13 <vedant.shrotria@chaosnative.com>

--- pkg/utils/scripts/network-loss-chaos.sh
+++ pkg/utils/scripts/network-loss-chaos.sh
@@ -6,15 +6,15 @@ echo "[Info]: Starting Network Loss Chaos ..."
 
 echo "PRIVATE_SSH_FILE_PATH: $PRIVATE_SSH_FILE_PATH"
 echo "[Info]: Connection information, IP: ${IP}, USER: ${USER}, PORT: ${PORT}"
-echo "[Info]: Chaos Command: tc qdisc replace dev ${NETWORK_INTERFACE} root netem loss ${NETWORK_PACKET_LOSS_PERCENTAGE} && sleep ${TOTAL_CHAOS_DURATION} && tc qdisc delete dev ${NETWORK_INTERFACE} root"
+echo "[Info]: Chaos Command: sudo tc qdisc replace dev ${NETWORK_INTERFACE} root netem loss ${NETWORK_PACKET_LOSS_PERCENTAGE} && sleep ${TOTAL_CHAOS_DURATION} && sudo tc qdisc delete dev ${NETWORK_INTERFACE} root"
 
 if [ -z "$PRIVATE_SSH_FILE_PATH" ]; then
 
     sshpass -p ${PASSWORD} ssh -o StrictHostKeyChecking=no ${USER}@${IP} -p ${PORT} -tt \
-	"tc qdisc replace dev ${NETWORK_INTERFACE} root netem loss ${NETWORK_PACKET_LOSS_PERCENTAGE} && sleep ${TOTAL_CHAOS_DURATION} && tc qdisc delete dev ${NETWORK_INTERFACE} root"
+	"sudo tc qdisc replace dev ${NETWORK_INTERFACE} root netem loss ${NETWORK_PACKET_LOSS_PERCENTAGE} && sleep ${TOTAL_CHAOS_DURATION} && sudo tc qdisc delete dev ${NETWORK_INTERFACE} root"
     
 else
-    ssh -o StrictHostKeyChecking=no -i "$PRIVATE_SSH_FILE_PATH" ${USER}@${IP} "tc qdisc replace dev ${NETWORK_INTERFACE} root netem loss ${NETWORK_PACKET_LOSS_PERCENTAGE} && sleep ${TOTAL_CHAOS_DURATION} && tc qdisc delete dev ${NETWORK_INTERFACE} root" 
+    ssh -o StrictHostKeyChecking=no -i "$PRIVATE_SSH_FILE_PATH" ${USER}@${IP} "sudo tc qdisc replace dev ${NETWORK_INTERFACE} root netem loss ${NETWORK_PACKET_LOSS_PERCENTAGE} && sleep ${TOTAL_CHAOS_DURATION} && sudo tc qdisc delete dev ${NETWORK_INTERFACE} root" 
 fi
 
 echo "[Info]: Chaos Completed ..."
