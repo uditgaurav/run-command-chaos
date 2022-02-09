a2bd6faa9f01a1cbc27d6f8ea262d1732e159cb7
Jonsy13 <vedant.shrotria@chaosnative.com>
2022-02-09 19:42:11 +0530

    Added some fix
    Signed-off-by: Jonsy13 <vedant.shrotria@chaosnative.com>

--- pkg/utils/scripts/http-chaos.sh
+++ pkg/utils/scripts/http-chaos.sh
@@ -5,24 +5,20 @@ echo "[Info]: Starting httpchaos..."
 echo "PRIVATE_SSH_FILE_PATH: $PRIVATE_SSH_FILE_PATH"
 echo "[Info]: Connection information, IP: ${IP}, USER: ${USER}, PORT: ${PORT}"
 
-echo "Starting toxiproxy server"
-# Running toxiproxy server in background
-toxiproxy-server > /dev/null 2>&1 &
-sleep 2s
-
+startProxyServerCommand="toxiproxy-server > /dev/null 2>&1 &"
 toxicCreateCommand="toxiproxy-cli create -l ${LISTEN_URL} --${STREAM_TYPE} ${STREAM_URL} service-down-toxic > /dev/null"
 toggleToxicCommand="toxiproxy-cli toggle service-down-toxic > /dev/null && sleep ${TOTAL_CHAOS_DURATION}"
-revertToxicCommand="toxiproxy-cli delete service-down-toxic && sudo kill -9 $(ps aux | grep [t]oxiproxy | awk '{print $2}')"
+revertToxicCommand="toxiproxy-cli delete service-down-toxic"
 
-echo "[Info]: Chaos Command: ${toxicCreateCommand} && ${toggleToxicCommand} && ${revertToxicCommand}"
+echo "[Info]: Chaos Command: ${toxicCreateCommand} && ${toggleToxicCommand} && ${revertToxicCommand} && sudo kill -9 $(ps aux | grep [t]oxiproxy | awk '{print $2}')"
 
 if [ -z "$PRIVATE_SSH_FILE_PATH" ]; then
 
     sshpass -p ${PASSWORD} ssh -o StrictHostKeyChecking=no ${USER}@${IP} -p ${PORT} -tt \
-	"${toxicCreateCommand} && ${toggleToxicCommand} && ${revertToxicCommand}"
+	"${startProxyServerCommand} && sleep 2 && ${toxicCreateCommand} && ${toggleToxicCommand} && ${revertToxicCommand} && sudo kill -9 $(ps aux | grep [t]oxiproxy | awk '{print $2}')"
     
 else
-    ssh -o StrictHostKeyChecking=no -i "$PRIVATE_SSH_FILE_PATH" ${USER}@${IP} "${toxicCreateCommand} && ${toggleToxicCommand} && ${revertToxicCommand}" 
+    ssh -o StrictHostKeyChecking=no -i "$PRIVATE_SSH_FILE_PATH" ${USER}@${IP} "${startProxyServerCommand} && sleep 2 && ${toxicCreateCommand} && ${toggleToxicCommand} && ${revertToxicCommand} && sudo kill -9 $(ps aux | grep [t]oxiproxy | awk '{print $2}')" 
 fi
 
 echo "[Info]: Chaos Completed ..."
